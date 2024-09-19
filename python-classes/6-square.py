4d87c6fad8a6c35d652cc898a3018e75bc492057
Heidge <axel.gore35@gmail.com>
2024-09-19 16:40:19 +0200

    Add condition for check if integers into tuple are positive

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -94,10 +94,10 @@ class Square():
         else:
             for index in range(0, self.__size):
                 for index2 in range(0, self.__position[0]):
-                    print(" ")
+                    print(" ", end="")
                     index2 += 1
                 for index3 in range(0, self.__size):
-                    print("#")
+                    print("#", end="")
                     index3 += 1
-                print("")
+                print("", end="\n")
                 index += 1
