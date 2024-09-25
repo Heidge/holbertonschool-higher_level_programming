d10d166b2ee6046e27b8f1f630e1af2d1e1cc84f
Heidge <axel.gore35@gmail.com>
2024-09-25 11:33:15 +0200

    Fix 404 status and message if endpoint undefined request

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -29,7 +29,8 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         else:
-            self.send_error(404, "Endpoint not found", None)
+            self.send_response(404)
+            self.send_error(404, "Endpoint not found")
             self.end_headers()
 
 
