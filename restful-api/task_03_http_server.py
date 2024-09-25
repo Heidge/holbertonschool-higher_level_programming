eb02cf630e91fe94e2fb0f7de10068c0340ff54a
Heidge <axel.gore35@gmail.com>
2024-09-25 11:14:22 +0200

    Add task 3 file

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,38 @@
+import http.server
+import socketserver
+import json
+
+PORT = 8086
+DATA = {"name": "John", "age": 30, "city": "New York"}
+INFOS = {"version": "1.0", "description": "A simple API built with http.server"}
+
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            json_data = bytes(json.dumps(DATA), "utf-8")
+            self.wfile.write(json_data)
+        elif self.path == "/status":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == "/infos":
+            self.send_response(200)
+            self.end_headers()
+            json_infos = bytes(json.dumps(INFOS), "utf-8")
+            self.wfile.write(json_infos)
+        elif self.path == "/":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"This is a simple API")
+        else:
+            self.send_error(404, "Endpoint not found", None)
+            self.end_headers()
+
+
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
